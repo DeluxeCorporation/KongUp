1ce273d8b2aee608683c2637b21d6fe354ed20e5
Parthiban, Arun <arun.parthiban@deluxe.com>
2016-10-30 15:32:35 -0500

    Adding Kong notifications

--- kong_up.py
+++ kong_up.py
@@ -6,14 +6,24 @@ import requests
 
 KONG_HOST = os.getenv("KONG_HOST")
 HOSTNAME = os.getenv("HOST_NAME")
+HIPCHAT_URL = os.getenv("HIPCHAT_URL")
 
 def add_to_kong(request_path,port):
     upstream_url = "http://" + HOSTNAME + ":" + port
     k = requests.post('http://' + KONG_HOST + ':8001/apis/', data={"upstream_url": upstream_url, "request_path": request_path, "strip_request_path": True})
     if k.status_code == 201:
         print("Successfully added", request_path, "to gateway")
+        notifier(True, request_path)
     else:
         print("Could not add api to gateway")
+        notifier(False, request_path)
+
+def notifier(is_successful, request_path):
+    if is_successful:
+        message = '{"color":"green","message":"${API} KongedUP (successful)","notify":true,"message_format":"text"}'.format(API=request_path)
+    else:
+        message = '{"color":"green","message":"${API} not KongedUP (failed)","notify":true,"message_format":"text"}'.format(API=request_path)
+    m = requests.post(HIPCHAT_URL, data=message, headers={"Content-Type": "application/json"})
 
 def listener():
     cli = Client(version='auto')
@@ -37,3 +47,4 @@ def event_handler(event):
 
 if __name__ == '__main__':
     listener()
+# "https://dlxcorp.hipchat.com/v2/room/3272529/notification?auth_token=JpVrfU5CRB5kejefhafWk33iPmm9MvyGFImzEZBO"
