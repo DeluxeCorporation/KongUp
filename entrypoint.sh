fb1ac417530da5ae82d42b38db03b1ac99da98fd
Parthiban, Arun <arun.parthiban@deluxe.com>
2017-07-27 15:36:25 -0500

    Adding support for Kong>=0.10.0 and environment merging

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,21 @@
-#!/bin/bash
+!/bin/bash
 
-if [[ -z $KONG_HOST ]] ; then
-    missing_vars="$missing_vars KONG_HOST"
+if [[ -z $DEV_KONG_HOST ]] ; then
+    missing_vars="$missing_vars DEV_KONG_HOST"
 fi
 
-if [[ -z $HOSTNAME ]] ; then
-    missing_vars="$missing_vars HOST_NAME"
+if [[ -z $QA_KONG_HOST ]] ; then
+    missing_vars="$missing_vars QA_KONG_HOST"
+fi
+
+if [[ -z $UAT_KONG_HOST ]] ; then
+    missing_vars="$missing_vars UAT_KONG_HOST"
+fi
+if [[ -z $XUAT_KONG_HOST ]] ; then
+    missing_vars="$missing_vars XUAT_KONG_HOST"
+fi
+if [[ -z $PREPROD_KONG_HOST ]] ; then
+    missing_vars="$missing_vars PREPROD_KONG_HOST"
 fi
 
 if [[ $missing_vars ]] ; then
--- kong_up.py
+++ kong_up.py
@@ -5,16 +5,23 @@ import os
 import requests
 import uuid
 import time
-KONG_HOST = os.getenv("KONG_HOST")
-HOSTNAME = os.getenv("HOST_NAME") if os.getenv("HOST_NAME") else socket.gethostname()
+KONGS = {"DEV" : os.getenv("DEV_KONG_HOST"),
+         "QA" : os.getenv("QA_KONG_HOST"),
+         "UAT": os.getenv("UAT_KONG_HOST"),
+         "XUAT":os.getenv("XUAT_KONG_HOST"),
+         "PREPROD":os.getenv("PREPROD_KONG_HOST"),
+         "PROD":os.getenv("PROD_KONG_HOST")}
+HOSTNAME = os.getenv("HOSTNAME")
 HIPCHAT_URL = os.getenv("HIPCHAT_URL")
 
 
-def get_api(uri):
+def get_api(uri, environment):
     '''
-    Return API if API exists, else False. Check only
+    Return API if API exists in the environment
+    specified, else False. Check only
     the first uri in the api.
     '''
+    KONG_HOST = KONGS[environment]
     apis = requests.get('http://' + KONG_HOST + ':8001/apis/').json()['data']
     for api in apis:
         if api['uris'][0] == uri:
@@ -22,13 +29,13 @@ def get_api(uri):
     return False
 
 
-def add_to_kong(uri, port):
+def add_to_kong(uri, port, environment):
     '''
-    Add uri to Kong on port and HOSTNAME
+    Add uri to Kong in environment on port 
     '''
     upstream_url = "http://" + HOSTNAME + ":" + port
-
-    api = get_api(uri)
+    KONG_HOST = KONGS[environment]
+    api = get_api(uri, environment)
 
     if api:
         api['upstream_url'] = upstream_url
@@ -36,24 +43,27 @@ def add_to_kong(uri, port):
         api = {
             "upstream_url": upstream_url,
             "uris": uri,
+            "name": uri[1:],
             "created_at": int(time.time())}
 
+    print("request" + str(api))
     k = requests.put('http://' + KONG_HOST + ':8001/apis/', data=api)
 
     if k.status_code == 201 or k.status_code == 200:
         print("Successfully added", uri, "to gateway")
-        notifier(True, uri)
-    elif k.status_code == 409:
+        notifier(True, uri, KONG_HOST)
+    else:
         print("Could not add api to gateway", k.json())
-        notifier(False, uri)
+        notifier(False, uri, KONG_HOST)
 
 
-def notifier(is_successful, uri):
+def notifier(is_successful, uri, KONG_HOST=''):
     '''
     Send a notification to hipchat
 
     :param is_successful: (Boolean) - True if api was added successfully, false otherwise
     :param uri: (String) - Request path to the api that was/wasn't added
+    :KONG_HOST: (String) - hostname of the server where kong is running
     '''
     gateway_link = "https://" + KONG_HOST + ":8243" + uri
     if is_successful:
@@ -76,7 +86,7 @@ def listener():
             try:
                 event_handler(event)
             except Exception as e:
-                notifier(False, e)
+                notifier(False, str(e))
                 continue
 
 def get_port_from_ip_table(pvt_c_id):
@@ -104,8 +114,9 @@ def event_handler(event):
         pvt_c_id = container['Config']['Labels'].get('io.rancher.container.ip')[:-3]
         port = get_port_from_ip_table(pvt_c_id)
         request_path = container['Config']['Labels'].get('GATEWAY_REQUEST_PATH')
+        environment = container['Config']['Labels'].get('ENVIRONMENT')
         if request_path: 
-            add_to_kong(request_path, port)
+            add_to_kong(request_path, port, environment)
 
 def rewire():
     cli = Client(version='auto')
@@ -118,8 +129,9 @@ def rewire():
                 pvt_c_id = container['Labels'].get('io.rancher.container.ip')[:-3]
                 port = get_port_from_ip_table(pvt_c_id)
                 request_path = container['Labels'].get('GATEWAY_REQUEST_PATH')
+                environment = container['Labels'].get('ENVIRONMENT')
                 if request_path: 
-                    add_to_kong(request_path, port)
+                    add_to_kong(request_path, port, environment)
         except Exception as e:
             notifier(False, str(e))
             continue
